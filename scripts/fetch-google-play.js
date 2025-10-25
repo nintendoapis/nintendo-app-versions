a0ca41322dcf0992de3cf4fea150c36386bdf82e
Ellie <ellie@fancy.org.uk>
2025-10-25 01:16:49 +0100

    Set Google Play country for entry

--- scripts/fetch-google-play.js
+++ scripts/fetch-google-play.js
@@ -3,7 +3,7 @@ import googleplay from 'google-play-scraper';
 const data = await googleplay.app({
     appId: process.argv[2],
     lang: 'en_GB',
-    country: 'GB',
+    country: process.argv[3] || 'GB',
 });
 
 if (data.released?.match(/^[a-z]+ \d+, \d+$/i)) {
